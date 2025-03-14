126b1b07ab529192ae0ba8808ad640d2bd64c8fc
heyseth <sethmillerp@gmail.com>
2025-03-14 13:35:07 -0700

    revert last commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 const rateLimit = require('express-rate-limit')
-const axios = require('axios')
 require('dotenv').config()
 const errorHandler = require('./middleware/error')
 
@@ -32,21 +31,4 @@ app.use('/api', require('./routes'))
 // Error handler middleware
 app.use(errorHandler)
 
-// Function to get EC2 public IP
-async function getEC2PublicIP() {
-  try {
-    const response = await axios.get('http://169.254.169.254/latest/meta-data/public-ipv4')
-    return response.data
-  } catch (error) {
-    // If not running on EC2 or cannot fetch IP, return localhost
-    return 'localhost'
-  }
-}
-
-app.listen(PORT, async () => {
-  const publicIP = await getEC2PublicIP()
-  const publicURL = `http://${publicIP}:${PORT}`
-  console.log(`Server running on port ${PORT}`)
-  console.log(`Public URL: ${publicURL}`)
-  console.log(`API URL: ${publicURL}/api`)
-})
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
